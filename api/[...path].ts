cce5bafe508181194f67d21f667ec631ada03f37
ziir0 <lorivaldo592@gmail.com>
2026-09-15 16:09:44 +0100

    Export Express app for Vercel

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,7 +1,3 @@
 import { createApp } from "../server/app";
 
-const app = createApp();
-
-export default function handler(req: any, res: any) {
-	return app(req, res);
-}
+export default createApp();
